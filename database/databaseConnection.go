9fbd2f964b535c2145a475d31064698f1ce1bc51
sencerarslan <m.sencerarslan@gmail.com>
2024-02-26 11:23:37 +0300

    update

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -39,6 +39,6 @@ func DBinstance() *mongo.Client {
 var Client *mongo.Client = DBinstance()
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	var collection *mongo.Collection = client.Database("go-admin").Collection(collectionName)
+	var collection *mongo.Collection = client.Database("qr-menu").Collection(collectionName)
 	return collection
 }
